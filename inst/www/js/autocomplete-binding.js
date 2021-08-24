02b811036a6458d2f828132cf3aab908734bddc9
dee-sea <65192413+dee-sea@users.noreply.github.com>
2021-08-24 11:30:45 +0200

    remove last change

--- inst/www/js/autocomplete-binding.js
+++ inst/www/js/autocomplete-binding.js
@@ -157,12 +157,7 @@ function autocomplete(inp) {
       if (currentFocus > -1 && x) {
         x[currentFocus].click();
       }
-    } else {
-      //ENTER key
-      e.preventDefault();
-      if (currentFocus > -1 && x) {
-        x[currentFocus].click();
-      }
+    } 
       
     if (x && x[currentFocus]) {
       var xot = x[currentFocus].offsetTop,
