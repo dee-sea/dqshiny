804900105833058809d0614a6da2ca0e1ef73210
dee-sea <65192413+dee-sea@users.noreply.github.com>
2021-08-24 14:50:07 +0200

    34

--- inst/www/js/autocomplete-binding.js
+++ inst/www/js/autocomplete-binding.js
@@ -107,7 +107,7 @@ function autocomplete(inp) {
       labeled = !arr.length;
 
     var onClick = function (ce) {
-      alert($(ce.target).data("value"));
+      alert($el.val($(ce.target).data("value")));
       $el.val($(ce.target).data("value")).trigger("change");
     };
 //-------------
